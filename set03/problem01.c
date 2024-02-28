2c4a4aee44ebd7f621774b06b6b7fe93ccd26f16
Janmikaj <jayamj.bsc23@rvu.edu.in>
2024-02-28 12:05:48 +0530

    test

---
+++ set03/problem01.c
@@ -0,0 +1,6 @@
+// 1. Write a program to find the distance between 2 points
+
+
+void input(float *x1, float *y1, float *x2, float *y2);
+float find_distance(float x1, float y1, float x2, float y2);
+void output(float x1, float y1, float x2, float y2, float distance);
